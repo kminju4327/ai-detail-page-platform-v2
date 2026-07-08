a1fc9009edffb81084bcda8361a33f4d2dfbf3e3
kminju4327 <kminju4327@gmail.com>
2026-07-08 15:30:06 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// 메인 앱: 최신 디자인의 DetailPageGenerator 컴포넌트를 렌더링합니다.
+// 해당 컴포넌트가 모든 상태/로직/UI를 포함하고 있습니다.
+
+import DetailPageGenerator from "./components/DetailPageGenerator.jsx";
+
+export default function App() {
+  return <DetailPageGenerator />;
+}
